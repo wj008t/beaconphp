389c7852b542eb779e52cd6e1bf88155277e50f8
wj008 <26029682@qq.com>
2017-12-21 23:08:59 +0800

    继续开发修改

--- www/assets/js/yee.select_dialog.js
+++ www/assets/js/yee.select_dialog.js
@@ -4,24 +4,33 @@
         var textBox = $('<input type="text" readonly="readonly"/>').insertAfter(qem);
         textBox.attr('class', qem.attr('class'));
         textBox.attr('style', qem.attr('style'));
+        textBox.data('val-for', '#' + qem.attr('id') + '_info');
         qem.hide();
-        qem.on('displayError', function (ev, data) {
-            textBox.addClass('error');
-        });
-        qem.on('displayDefault displayValid', function (ev, data) {
-            textBox.removeClass('error');
-        });
-        textBox.on('mouseenter', function () {
-            if (typeof(qem.setDefault) == 'function') {
-                qem.setDefault();
-            }
-        });
+        if (typeof textBox.setError == 'function') {
+            qem.on('displayError', function (ev, data) {
+                textBox.setError();
+            });
+            qem.on('displayDefault', function (ev, data) {
+                textBox.setDefault();
+            });
+            qem.on('displayValid', function (ev, data) {
+                textBox.setValid();
+            });
+            textBox.on('mouseenter', function () {
+                if (typeof(qem.setDefault) == 'function') {
+                    qem.setDefault();
+                }
+            });
+        }
         var button = $('<a class="yee-btn" href="javascript:;" yee-module="dialog" style="display: inline-block;">选择</a>').insertAfter(textBox);
         button.data('href', option.href || '');
         button.on('select_dialog_data', function (ev, data) {
             if (data && data.value && data.text) {
                 qem.val(data.value);
                 textBox.val(data.text);
+                if (typeof textBox.setDefault == 'function') {
+                    textBox.setDefault();
+                }
             }
         });
         Yee.update(button.parent());
