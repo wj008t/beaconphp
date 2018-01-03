178c504e9d45961b649d8c1ee11344dcc0c4235a
wj008 <26029682@qq.com>
2018-01-04 05:47:09 +0800

    继续开发修改

--- www/swoole.php
+++ www/swoole.php
@@ -14,11 +14,13 @@ Route::register('home');
 Route::register('service');
 Route::register('flow');
 Route::register('admin');
-
+if (!IS_CLI) {
+    exit;
+}
 $http = new swoole_http_server("127.0.0.1", 9501);
 $http->on('request', function ($request, $response) {
     $url = $request->server['request_uri'];
-    if (Route::runStatic($url, $request, $response, ['static/', 'assets/', 'upfiles/'])) {
+    if (Route::runStatic($url, $request, $response, ['static/', 'assets/', 'upfiles/', 'favicon.ico'])) {
         return;
     }
     Route::run($url, $request, $response);
